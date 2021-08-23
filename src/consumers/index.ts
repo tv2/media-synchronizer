c6740395dcc66c55f1759c5ec6d7cca9d5efc2e4
Jesper Leerberg Madsen <jlem@tv2.dk>
2021-08-23 10:35:52 +0200

    prettier + improved logging (#4)

--- src/consumers/index.ts
+++ src/consumers/index.ts
@@ -3,10 +3,19 @@ import { MediaWatcherConsumer } from './media-watcher.consumer'
 import { ControllerConsumer } from './controller.consumer'
 import { resolve } from 'path'
 import { FileTransfererConsumer } from './file-transferer.consumer'
+import { logger } from '../lib/utilities/logger'
+import { name, version } from '../../package.json'
+import { environment } from '../lib/utilities/environment'
 import { normalizePath } from '../lib/utilities/filesystem'
 
 const sourcePath = normalizePath(resolve(process.env.SOURCE_PATH || './__source__'))
 const targetPath = normalizePath(resolve(process.env.TARGET_PATH || './__target__'))
+
+logger.info(`Starting up ${name}`)
+logger.info(`Version: ${version}`)
+logger.info(`Environment: ${environment}`)
+logger.info(`SOURCE_PATH: ${sourcePath}`)
+logger.info(`TARGET_PATH: ${targetPath}`)
 
 // Setup media watcher
 const mediaWatcher = new MediaWatcherConsumer({ sourcePath: sourcePath })
--- src/lib/utilities/logger/console.transport.ts
+++ src/lib/utilities/logger/console.transport.ts
@@ -1,27 +1,25 @@
 import { transports, format as wformat } from 'winston'
 import { environment, Environment } from '../environment'
 
-const prettyJson = wformat.printf(info => {
+const prettyJson = wformat.printf((info) => {
   if (typeof info.message === 'object') {
     console.log('')
     console.log(info.message)
     return ''
   }
-  return `[ ${ info.timestamp } ] [ ${ info.level } ]  ${info.message}`
+  return `[ ${info.timestamp} ] [ ${info.level} ]  ${info.message}`
 })
 
-const format = environment === Environment.Local ?
-  wformat.combine(
-    wformat.timestamp(),
-    wformat.colorize(),
-    wformat.prettyPrint(),
-    wformat.splat(),
-    wformat.simple(),
-    prettyJson,
-  ) :
-  wformat.combine(
-    wformat.timestamp(),
-    wformat.json(),
-  )
+const format =
+  environment === Environment.Local
+    ? wformat.combine(
+        wformat.timestamp(),
+        wformat.colorize(),
+        wformat.prettyPrint(),
+        wformat.splat(),
+        wformat.simple(),
+        prettyJson
+      )
+    : wformat.combine(wformat.timestamp(), wformat.json())
 
 export const transport = new transports.Console({ format })
